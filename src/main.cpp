e047307845b9056fd30ff48b36893f42a6642a34
moisesfilho <moises.son@gmail.com>
2021-03-18 19:11:05 -0300

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,81 @@
+#include <Arduino.h>
+#include <Mouse.h>
+
+const byte PIN_BUTTON_A = 2;
+const byte PIN_BUTTON_B = 3;
+const byte PIN_BUTTON_C = 4;
+const byte PIN_BUTTON_D = 5;
+const byte PIN_BUTTON_E = 6;
+const byte PIN_BUTTON_F = 7;
+const byte PIN_ANALOG_X = 0;
+const byte PIN_ANALOG_Y = 1;
+
+const int ledPin = 13;
+
+boolean mouseIsActive = false;
+int lastSwitchState = LOW;
+
+int responseDelay = 5;
+int range = 10;
+
+void setup()
+{
+  pinMode(PIN_BUTTON_A, INPUT);
+  pinMode(ledPin, OUTPUT);
+
+  Mouse.begin();
+}
+
+void loop()
+{
+  int switchState = digitalRead(PIN_BUTTON_A);
+
+  if (switchState != lastSwitchState)
+  {
+    if (switchState == LOW)
+    {
+      mouseIsActive = !mouseIsActive;
+      digitalWrite(ledPin, mouseIsActive);
+    }
+  }
+  
+  lastSwitchState = switchState;
+
+  int x = map(analogRead(PIN_ANALOG_X), 0, 750, -1 * range, range);
+  int y = map(analogRead(PIN_ANALOG_Y), 0, 750, range, -1 * range);
+
+  if (mouseIsActive)
+  {
+    Mouse.move(x, y, 0);
+  }
+
+  if (digitalRead(PIN_BUTTON_D) == LOW)
+  {
+    if (!Mouse.isPressed(MOUSE_LEFT))
+    {
+      Mouse.press(MOUSE_LEFT);
+    }
+  }
+  else
+  {
+    if (Mouse.isPressed(MOUSE_LEFT))
+    {
+      Mouse.release(MOUSE_LEFT);
+    }
+  }
+  if (digitalRead(PIN_BUTTON_B) == LOW)
+  {
+    if (!Mouse.isPressed(MOUSE_RIGHT))
+    {
+      Mouse.press(MOUSE_RIGHT);
+    }
+  }
+  else
+  {
+    if (Mouse.isPressed(MOUSE_RIGHT))
+    {
+      Mouse.release(MOUSE_RIGHT);
+    }
+  }
+  delay(responseDelay);
+}
